c10397ae24444c0bdffe8f1b3a8b66bd105c50e1
Edo <75941214+EdoGaming28@users.noreply.github.com>
2021-10-19 19:13:25 +0700

    Create UpdateTask.php

---
+++ src/Zedstar16/OnlineTime/UpdateTask.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zedstar16\OnlineTime;
+
+use pocketmine\scheduler\Task;
+
+class UpdateTask extends Task{
+
+    public function __construct(Main $pl){
+     $this->pl = $pl;
+    }
+
+    public function onRun($tick){
+     $lb = $this->pl->getLeaderBoard();
+     $list = $this->pl->getParticles();
+     foreach($list as $particle){
+      $particle->setText($lb);
+     }
+    }
+
+}
